be18f1d8d2b79ba0f763facf6abe010f942a4239
oshacker <user@userdeMacBook-Pro.local>
2020-03-27 16:13:06 +0800

    init

---
+++ src/cn/coderap/test/HashMap/Test.java
@@ -0,0 +1,13 @@
+package cn.coderap.test.HashMap;
+
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+        HashMap<String, Double> map = new HashMap<>();
+        map.put("k1",1.0);
+        map.put("k2",2.0);
+        map.put("k3",3.0);
+    }
+
+}
